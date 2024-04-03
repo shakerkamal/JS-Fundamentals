bc504752d6e50a0640dd3a5654d2a29c70d7b918
shakerkamal <shakerkamal18@gmail.com>
2024-04-03 21:03:16 +0200

    arraytuples

---
+++ arraysAndTuples.ts
@@ -0,0 +1,11 @@
+let strArr: string[] = ['Hello','Hi']
+let strArr1: Array<string> = ['Good', 'Morning']
+
+let anyArr: any[] = ['danke',122, true]
+
+let myTuple: [string, number] = ['Hi',35]
+
+console.log(myTuple[0])
+console.log(myTuple[1])
+// myTuple[2] = "Tree";
+// console.log(myTuple[2])
